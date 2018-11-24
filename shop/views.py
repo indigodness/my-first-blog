cb68c3213f868c5d9ad85ae601fc924285a0d144
Alisher <alisher.relito@gmail.com>
2018-11-24 19:35:52 +0600

    Modified templates to display posts from database.

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'shop/post_list.html', {})
+    posts = Product.objects.all()
+    return render(request, 'shop/post_list.html', {'posts': posts})
