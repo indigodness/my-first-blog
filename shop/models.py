e5193d908ccc6519d9cd2a115cc129342c1c6781
Alisher <alisher.relito@gmail.com>
2018-12-13 11:06:38 +0600

    PLS

--- shop/models.py
+++ shop/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+import os
 
 class Product(models.Model):
    #  author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length = 50)
-    print(settings.MEDIA_ROOT+"/456339.jpg")
-    photo = models.ImageField(default=settings.MEDIA_ROOT+"/456339.jpg")
+    print("THIS IS PRINT",settings.MEDIA_ROOT+"/456339_XxqsMMx.jpg")
+#    os.chdir(settings.MEDIA_ROOT)
+    print(os.getcwd())
+    photo = models.ImageField(settings.MEDIA_ROOT+"/456339_XxqsMMx.jpg")
     description = models.TextField()
     dept = models.CharField(max_length = 50)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
     def __str__(self):
         return self.name
--- shop/urls.py
+++ shop/urls.py
@@ -4,13 +4,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-]
+               url(r'^$', views.main, name='main'),
+               url(r'^$', views.about, name='about'),
+               url(r'^$', views.stores, name='stores'),
+               ]
+
 
 #if settings.DEBUG:
-#    urlpatterns += [
-#        url(r'^media/(?P<path>.*)$', serve, {
-#            'document_root':settings.MEDIA_ROOT,
-#        }),
-#        
-#    ]
+#urlpatterns += [
+                    #url(r'^media/(?P<path>.*)$', {
+            #'document_root':settings.MEDIA_ROOT,
+            # }),
+                    #
+                    #    ]
