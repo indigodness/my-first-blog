d888551fbfa2fd1d907a6f41af4d621bb1fd05a1
Alisher <alisher.relito@gmail.com>
2018-12-14 13:48:38 +0600

    LAST

--- shop/urls.py
+++ shop/urls.py
@@ -2,11 +2,20 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
-               url(r'', views.main, name='main'),
-               url(r'^$', views.about, name='about'),
-               url(r'^$', views.stores, name='stores'),
+               path('', views.main, name='main'),
+               path('about/', views.about, name='about'),
+               path('stores/', views.stores, name='stores'),
+               path('bakery/', views.bakery, name='bakery'),
+               path('cereal/', views.cereal, name='cereal'),
+               path('diary/', views.diary, name='diary'),
+               path('drinks/', views.drinks, name='drinks'),
+               path('frozen/', views.frozen, name='frozen'),
+               path('meat/', views.meat, name='meat'),
+               path('snack/', views.snack, name='snack'),
+               path('vegan/', views.vegan, name='vegan'),
                ]
 
 #if settings.DEBUG:
--- shop/views.py
+++ shop/views.py
@@ -14,3 +14,35 @@ def about(request):
 def stores(request):
     posts = Product.objects.all()
     return render(request, 'shop/stores.html', {'posts': posts})
+
+def bakery(request):
+    posts = Product.objects.all()
+    return render(request, 'shop/bakery.html', {'posts': posts})
+
+def cereal(request):
+    posts = Product.objects.all()
+    return render(request, 'shop/cereal.html', {'posts': posts})
+
+def diary(request):
+    posts = Product.objects.all()
+    return render(request, 'shop/diary.html', {'posts': posts})
+
+def drinks(request):
+    posts = Product.objects.all()
+    return render(request, 'shop/drinks.html', {'posts': posts})
+
+def frozen(request):
+    posts = Product.objects.all()
+    return render(request, 'shop/frozen.html', {'posts': posts})
+
+def meat(request):
+    posts = Product.objects.all()
+    return render(request, 'shop/meat.html', {'posts': posts})
+
+def snack(request):
+    posts = Product.objects.all()
+    return render(request, 'shop/snack.html', {'posts': posts})
+
+def vegan(request):
+    posts = Product.objects.all()
+    return render(request, 'shop/vegan.html', {'posts': posts})
